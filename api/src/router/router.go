f728a99d9b75007aa46a840e8bad70c6e2341fa4
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-03-30 17:06:58 -0300

    Inserindo rotas dentro do Router

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,7 +1,13 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/routes"
+
+	"github.com/gorilla/mux"
+)
 
 func Generate() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+
+	return routes.Config(r)
 }
--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -1,10 +1,24 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Route struct {
 	Uri                     string
 	Method                  string
 	Function                func(http.ResponseWriter, *http.Request)
 	IsRequireAuthentication bool
+}
+
+func Config(r *mux.Router) *mux.Router {
+	routes := usersRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.Uri, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
