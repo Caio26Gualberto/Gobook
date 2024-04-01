32c5380fc3b4ed462514050f939ea5e4fef63cae
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-04-01 00:44:15 -0300

    package repositories

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -1,9 +1,35 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/db"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário!"))
+	requestBody, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUserRepository(db)
+	repository.Create(user)
 }
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
